fd1d5f16117e86e16307a48ae4ebd1b1f1649575
Danimartinezz <daniel.martinez094@mymdc.net>
2022-06-01 15:28:45 +0000

    My final version

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", function() {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = function() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let where = [
+    "because was hungy",
+    "because was angry",
+    "because was distracted",
+    "because was bored"
+  ];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whereindex = Math.floor(Math.random() * when.length);
+  let whenindex = Math.floor(Math.random() * where.length);
+
+  return (
+    who[whoindex] +
+    " " +
+    what[whatindex] +
+    " " +
+    where[whereindex] +
+    " " +
+    when[whenindex]
+  );
 };
